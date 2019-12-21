5e1be69fff2496a72e71587d5851302b9a192850
Haythem-stack <haytheasidi@gmail.com>
2019-12-21 18:53:26 +0100

    Initial Authors Routes

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ if(process.env.NODE_ENV !== 'production'){
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
-const indexRouter = require('./routes/index')
+const bodyParser = require('body-parser')
+
+const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors');
 
 
 
@@ -15,15 +18,18 @@ app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false}))
 
 // mongoose and mongoDB
 const mongoose = require('mongoose');
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
 const db = mongoose.connection;
 db.on('error', error => console.log(error));
 db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter);
+app.use('/authors', authorRouter)
+
 app.listen(process.env.PORT || 3000, () => {
     console.log('up and running')
 })
