571c5430d4bddc6ac17acfda9212e39eb2b072d8
Haythem-stack <haytheasidi@gmail.com>
2019-12-26 10:38:44 +0100

    books/index/new

--- routes/authors.js
+++ routes/authors.js
@@ -1,44 +1,44 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 const Author = require('../models/author')
+
 // All Authors Route
 router.get('/', async (req, res) => {
-    let searchOptions = {}
-    if (req.query.name != null && req.query.name !== ''){
-        searchOptions.name = new RegExp(req.query.name, 'i')
-    }
-    try{
-        const authors = await Author.find({searchOptions})
-        res.render('authors/index', { authors: authors, searchOptions:req.query})
-    } catch{
-        res.redirect('/')
-    }
-    
+  let searchOptions = {}
+  if (req.query.name != null && req.query.name !== '') {
+    searchOptions.name = new RegExp(req.query.name, 'i')
+  }
+  try {
+    const authors = await Author.find(searchOptions)
+    res.render('authors/index', {
+      authors: authors,
+      searchOptions: req.query
+    })
+  } catch {
+    res.redirect('/')
+  }
 })
 
-// New Authour route
-
+// New Author Route
 router.get('/new', (req, res) => {
-    res.render('authors/new', { author: new Author()})
+  res.render('authors/new', { author: new Author() })
 })
 
-
-// Create author route
+// Create Author Route
 router.post('/', async (req, res) => {
-    const author = new Author({
-        name: req.body.name
-    })
-   try{
+  const author = new Author({
+    name: req.body.name
+  })
+  try {
     const newAuthor = await author.save()
-    //res.redirect(`authors/${newAuthor.id}`)
-    res.redirect('authors')
-   } catch {
+    // res.redirect(`authors/${newAuthor.id}`)
+    res.redirect(`authors`)
+  } catch {
     res.render('authors/new', {
-        author: author,
-        errorMessage: 'Error creating author'
+      author: author,
+      errorMessage: 'Error creating Author'
     })
-   }
-    
-});
+  }
+})
 
-module.exports = router;
+module.exports = router
